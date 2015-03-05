28d9521a85cb00f91b4ec12dec974586ab86027c
Kevin Chen <chenkc805@gmail.com>
2015-03-04 22:28:25 -0800

    Fixed Indentations

--- proj1/ngordnet/TimeSeries.java
+++ proj1/ngordnet/TimeSeries.java
@@ -9,7 +9,7 @@ public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
 
     /** Constructs a new empty TimeSeries. */
     public TimeSeries() {
-      this.tSeries = new TreeMap<Integer, T>();
+        this.tSeries = new TreeMap<Integer, T>();
     }
 
     /** Returns the years in which tSeries time series is valid. Doesn't really
@@ -22,58 +22,58 @@ public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
     /** Creates a copy of TS, but only between STARTYEAR and ENDYEAR. 
      * inclusive of both end points. */
     public TimeSeries(TimeSeries<T> ts, int startYear, int endYear) {
-      this.tSeries = new TreeMap<Integer, T>(ts.subMap(startYear, endYear));
+        this.tSeries = new TreeMap<Integer, T>(ts.subMap(startYear, endYear));
     }
 
     /** Creates a copy of TS. */
     public TimeSeries(TimeSeries<T> ts) {
-      this.tSeries = new TreeMap<Integer, T>(ts.subMap(ts.firstKey(), ts.lastKey()));
+        this.tSeries = new TreeMap<Integer, T>(ts.subMap(ts.firstKey(), ts.lastKey()));
     }
 
     /** Returns the quotient of this time series divided by the relevant value in ts.
       * If ts is missing a key in this time series, return an IllegalArgumentException. */
     public TimeSeries<Double> dividedBy(TimeSeries<? extends Number> ts) {
-      TimeSeries<Double> result = new TimeSeries();
-      for (Integer key: tSeries.keySet()) {
-        if (!ts.containsKey(key)) {
-          throw new IllegalArgumentException();
-        } else {
-          result.put(key, tSeries.get(key).doubleValue() / ts.get(key).doubleValue());
+        TimeSeries<Double> result = new TimeSeries();
+        for (Integer key: tSeries.keySet()) {
+            if (!ts.containsKey(key)) {
+            throw new IllegalArgumentException();
+            } else {
+                result.put(key, tSeries.get(key).doubleValue() / ts.get(key).doubleValue());
+            }
         }
-      }
-      return result;
+        return result;
     }
 
     /** Returns the sum of this time series with the given ts. The result is a 
       * a Double time series (for simplicity). */
     public TimeSeries<Double> plus(TimeSeries<? extends Number> ts) {
-      TimeSeries<Double> result = new TimeSeries();
-      for (Integer key: tSeries.keySet()) {
-        if (!ts.containsKey(key)) {
-          throw new IllegalArgumentException();
-        } else {
-          result.put(key, tSeries.get(key).doubleValue() + ts.get(key).doubleValue());
+        TimeSeries<Double> result = new TimeSeries();
+        for (Integer key: tSeries.keySet()) {
+            if (!ts.containsKey(key)) {
+            throw new IllegalArgumentException();
+            } else {
+                result.put(key, tSeries.get(key).doubleValue() + ts.get(key).doubleValue());
+            }
         }
-      }
-      return result;
+        return result;
     }
 
     /** Returns all years for this time series (in any order). */
     public Collection<Number> years() {
-      Collection<Number> result = new HashSet<Number>();
-      for (Integer key: tSeries.keySet()) {
-        result.add(key);
-      }
-      return result;
+        Collection<Number> result = new HashSet<Number>();
+        for (Integer key: tSeries.keySet()) {
+            result.add(key);
+        }
+        return result;
     }
 
     /** Returns all data for this time series (in any order). */
     public Collection<Number> data() {
-      Collection<Number> result = new HashSet<Number>();
-      for (Integer key: tSeries.keySet()) {
-        result.add(tSeries.get(key));
-      }
-      return result;
+        Collection<Number> result = new HashSet<Number>();
+        for (Integer key: tSeries.keySet()) {
+            result.add(tSeries.get(key));
+        }
+        return result;
     }
 }
 
--- proj1/ngordnet/WordNet.java
+++ proj1/ngordnet/WordNet.java
@@ -6,49 +6,49 @@ import java.util.*;
 
 public class WordNet {
 
-  private Map<Integer, ArrayList<String>> synsetNouns;
-  private Digraph hyponym;
-  private Set<String> nouns;
+    private Map<Integer, ArrayList<String>> synsetNouns;
+    private Digraph hyponym;
+    private Set<String> nouns;
 
   /** Creates a WordNet using files form SYNSETFILENAME and HYPONYMFILENAME */
-  public WordNet(String synsetFilename, String hyponymFilename) {
-    In synsetScanner = new In(synsetFilename);
+    public WordNet(String synsetFilename, String hyponymFilename) {
+        In synsetScanner = new In(synsetFilename);
 
-    synsetNouns = new HashMap<Integer, ArrayList<String>>();
-    nouns = new HashSet<String>();
+        synsetNouns = new HashMap<Integer, ArrayList<String>>();
+        nouns = new HashSet<String>();
 
-    while (synsetScanner.hasNextLine()) {
-      ArrayList<String> synsetArrayList = new ArrayList<String>();
-      String row = synsetScanner.readLine();
-      String[] tokens = row.split(",");
-      String[] synset = tokens[1].split(" ");
-      for (int i = 0; i < synset.length; i++) {
-        synsetArrayList.add(synset[i]);
-        nouns.add(synset[i]);
-      }
-      synsetNouns.put(Integer.parseInt(tokens[0]),synsetArrayList);
-    }
+        while (synsetScanner.hasNextLine()) {
+            ArrayList<String> synsetArrayList = new ArrayList<String>();
+            String row = synsetScanner.readLine();
+            String[] tokens = row.split(",");
+            String[] synset = tokens[1].split(" ");
+            for (int i = 0; i < synset.length; i++) {
+                synsetArrayList.add(synset[i]);
+                nouns.add(synset[i]);
+            }
+            synsetNouns.put(Integer.parseInt(tokens[0]),synsetArrayList);
+        }
 
-    In hyponymScanner = new In(hyponymFilename);
-    hyponym = new Digraph(synsetNouns.size());
-    while (hyponymScanner.hasNextLine()) {
-      String row = hyponymScanner.readLine();
-      String[] tokens = row.split(",");
-      for (int i = 1; i < tokens.length; i++) {
-        hyponym.addEdge(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[i]));
-      }
-    }
+        In hyponymScanner = new In(hyponymFilename);
+        hyponym = new Digraph(synsetNouns.size());
+        while (hyponymScanner.hasNextLine()) {
+            String row = hyponymScanner.readLine();
+            String[] tokens = row.split(",");
+            for (int i = 1; i < tokens.length; i++) {
+                hyponym.addEdge(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[i]));
+            }
+        }
   }
 
   /* Returns true if NOUN is a word in some synset. */
-  public boolean isNoun(String noun) {
-    return nouns.contains(noun);
-  }
+    public boolean isNoun(String noun) {
+        return nouns.contains(noun);
+    }
 
   /* Returns the set of all nouns. */
-  public Set<String> nouns() {
-    return nouns;
-  }
+    public Set<String> nouns() {
+        return nouns;
+    }
 
   /** Returns the set of all hyponyms of WORD as well as all synonyms of
     * WORD. If WORD belongs to multiple synsets, return all hyponyms of
@@ -56,21 +56,21 @@ public class WordNet {
     * Do not include hyponyms of synonyms.
     */
   public Set<String> hyponyms(String word) {
-    Set<String> hyponyms = new HashSet<String>();
-    Set<Integer> ids = new HashSet<Integer>();
-    for (Integer key: synsetNouns.keySet()) {
-      if (synsetNouns.get(key).contains(word)) {
-        ids.add(key);
-      }
+        Set<String> hyponyms = new HashSet<String>();
+        Set<Integer> ids = new HashSet<Integer>();
+        for (Integer key: synsetNouns.keySet()) {
+            if (synsetNouns.get(key).contains(word)) {
+                ids.add(key);
+            }
+        }
+        Set<Integer> descendants = GraphHelper.descendants(hyponym, ids);
+        for (Integer j: descendants) {
+            for (String value: synsetNouns.get(j)) {
+                hyponyms.add(value);
+            }
+        }
+        return hyponyms;
     }
-    Set<Integer> descendants = GraphHelper.descendants(hyponym, ids);
-    for (Integer j: descendants) {
-      for (String value: synsetNouns.get(j)) {
-        hyponyms.add(value);
-      }
-    }
-    return hyponyms;
-  }
 }
 
 
